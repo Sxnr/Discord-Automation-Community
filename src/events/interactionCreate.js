76ea862a0826fa40ae82f8f1eb96f8310821e6ee
Sxnr <franciscocarrera956@gmail.com>
2026-03-28 00:24:05 -0300

    fix bug

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -8,7 +8,7 @@ const { buildWarnPanel } = require('../commands/admin/warn');
 const { buildHistoryPanel } = require('../commands/admin/mod');
 const { buildLeaderboard } = require('../commands/utility/leaderboard');
 const { buildEmbed, buildVoteRow } = require('../utils/suggestHelpers');
-const { checkAndUnlock } = require('../utils/achievements');
+const { checkAndUnlock } = require('../economy/achievements');
 
 
 // ══════════════════════════════════════════════════════════
